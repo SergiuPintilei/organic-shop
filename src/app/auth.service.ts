f54d8d5577be2908b2a541a51f8c999f83f4887b
Sergiu Pintilei <sergiu.pintilei@strongbytes.ro>
2017-10-03 12:55:48 +0300

    show/hide the admin links

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,14 +1,18 @@
+import { UserService } from './user.service';
+import { AppUser } from './models/app-user';
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
 import { ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/of';
 
 @Injectable()
 export class AuthService {
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) {
+  constructor(private userService: UserService, private afAuth: AngularFireAuth, private route: ActivatedRoute) {
     this.user$ = afAuth.authState;
   }
 
@@ -21,6 +25,17 @@ export class AuthService {
 
   logout() {
     this.afAuth.auth.signOut();
+  }
+
+  get appUser$(): Observable<AppUser> {
+    return this.user$
+      .switchMap(user => {
+        if (user) {
+          return this.userService.get(user.uid);
+        } else {
+          return Observable.of(null);
+        }
+      });
   }
 
 }
