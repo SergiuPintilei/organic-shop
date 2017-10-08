dc623bec57027ba88168b860d345260cc1d7b700
Sergiu Pintilei <sergiu.pintilei@strongbytes.ro>
2017-10-08 21:35:20 +0300

    manage orders and view my orders

--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class OrderService {
 
-  constructor(private shoppingCartService: ShoppingCartService, private db: AngularFireDatabase) { }
+  constructor(private db: AngularFireDatabase, private shoppingCartService: ShoppingCartService) { }
 
   async placeOrder(order) {
     let result = await this.db.list('/orders').push(order);
@@ -13,4 +13,16 @@ export class OrderService {
     return result;
   }
 
+  getOrders() {
+    return this.db.list('/orders');
+  }
+
+  getOrdersByUser(userId: string) {
+    return this.db.list('/orders', {
+      query: {
+        orderByChild: 'userId',
+        equalTo: userId
+      }
+    });
+  }
 }
